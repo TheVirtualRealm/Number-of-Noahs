84b2e27102539b772893e47f6e22793c2068376e
TheVirtualRealm <70163600+TheVirtualRealm@users.noreply.github.com>
2020-12-02 19:13:04 -0500

    Add files via upload

---
+++ INCREMENT AND DECREMENT/lab1_2.js
@@ -0,0 +1,23 @@
+var numberOfApples = 10;
+document.write("The number of apples I have is" + numberOfApples + "<br>");
+numberOfApples++;
+document.write("The number of apples I have is" + numberOfApples + "<br>");
+numberOfApples++;
+document.write("The number of apples I have is" + numberOfApples + "<br>");
+numberOfApples++;
+document.write("The number of apples I have is" + numberOfApples + "<br>");
+numberOfApples++;
+document.write("The number of apples I have is" + numberOfApples + "<br>");
+numberOfApples++;
+document.write("The number of apples I have is" + numberOfApples + "<br>");
+numberOfApples++;
+
+document.write("<br><br>")
+
+var numberOfNoah = 100;
+document.write("There are " + --numberOfNoah + " noahs in the room. <br>")
+document.write("There are " + --numberOfNoah + " noahs in the room. <br>")
+document.write("There are " + --numberOfNoah + " noahs in the room. <br>")
+document.write("There are " + --numberOfNoah + " noahs in the room. <br>")
+document.write("There are " + --numberOfNoah + " noahs in the room. <br>")
+document.write("Created by Mr. Noah.")
